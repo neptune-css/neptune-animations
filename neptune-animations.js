0343e2275dc3b0cc30641b05183f32fbe8e21d85
CGWebDev2003 <developer@colingrahm.de>
2023-06-17 09:38:56 +0200

    Add animation class

---
+++ neptune-animations.js
@@ -0,0 +1,40 @@
+export default class NeptuneAnimate {
+	constructor(element, animationClass) {
+	  this.element = element;
+	  this.animationClass = animationClass;
+	  this.animationEndEvent = this.getAnimationEndEvent();
+
+	  this.element.addEventListener(this.animationEndEvent, this.handleAnimationEnd.bind(this));
+	}
+
+	getAnimationEndEvent() {
+	  const el = document.createElement("div");
+	  const animations = {
+		animation: "animationend",
+		OAnimation: "oAnimationEnd",
+		MozAnimation: "animationend",
+		WebkitAnimation: "webkitAnimationEnd"
+	  };
+
+	  for (const key in animations) {
+		if (el.style[key] !== undefined) {
+		  return animations[key];
+		}
+	  }
+
+	  return "animationend";
+	}
+
+	handleAnimationEnd() {
+	  this.element.classList.remove(this.animationClass);
+	  this.element.removeEventListener(this.animationEndEvent, this.handleAnimationEnd);
+	}
+
+	startAnimation() {
+	  this.element.classList.add(this.animationClass);
+	}
+
+	stopAnimation() {
+		this.element.classList.remove(this.animationClass);
+	}
+  }
